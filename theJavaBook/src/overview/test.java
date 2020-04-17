d1e3b3fc50d2d6a8fab7cff1864ff980294f3db3
Pramod Jana <32437635+PramodJana@users.noreply.github.com>
2020-04-17 18:54:28 +0530

    started to read a book

---
+++ theJavaBook/src/overview/test.java
@@ -0,0 +1,11 @@
+package overview;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a simple java program");
+
+	}
+
+}
